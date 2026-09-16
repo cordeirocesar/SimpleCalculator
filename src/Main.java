ef56c0e3b01a7d27e1221b99763f9d782f594923
Cordeiro Cesar <cesarcordeiromphc@gmail.com>
2026-09-16 15:54:43 +0200

    add Scanner setup

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
